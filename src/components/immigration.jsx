086074103dad74c8ea500908018cade24e7fb094
Prabu1504 <prabupalani1999@gamil.com>
2024-04-29 22:51:15 +0530

    head

--- src/components/immigration.jsx
+++ src/components/immigration.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-const Immigration = () => 
-{
-    return(
-        <>     
-            <p>contactUs</p>
-        </>
-    )
+import  { useEffect,React, useState } from 'react';
+import image1 from "../components/assets/immigration1.jpg";
+import image2 from "../components/assets/immigration2.jpg"
 
+
+
+
+const Immigration = () => {
+  const[imageindex,setimageindes]=useState(0)
+  const imagearray=[image1,image2]
+
+useEffect(()=>{
+  const interval=setInterval(()=>{
+    setimageindes((prev)=>(prev+imagearray.length+1)%imagearray.length)
+  },3000)
+  return ()=> clearInterval(interval)
+
+},[imagearray])
+  useEffect(()=>{
+    AOS.init({duration:1000})
+  },[])
+  return (
+    <div className="gap-10  w-full h-screen">
+     <img src={imagearray[imageindex]} alt="logo" className='scroll-smooth  w-5 h-5' />
+     
+    </div>
+  )
 }
 
-export default Immigration ;
+export default Immigration;
