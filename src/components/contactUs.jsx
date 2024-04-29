52d45ad7740346159aa73c6929026bb11f0a7306
MadhumithaBalasubramani <madhubalu2082001@gmail.com>
2024-04-29 14:20:07 +0530

    msg

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -1,12 +1,66 @@
 import React from "react";
-const ContactUs = () => 
-{
-    return(
-        <>     
-            <p>contactUs</p>
-        </>
-    )
 
+const ContactUs = () => {
+  return (
+    <section className="bg-gradient-animation bg-center">
+      <div className="flex justify-center">
+        <div className="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+            <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+              <form action="#" className="space-y-4">
+                <div>
+                  <label className="sr-only" htmlFor="name">Name</label>
+                  <input
+                    className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                    placeholder="Name"
+                    type="text"
+                    id="name"
+                  />
+                </div>
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                  <div>
+                    <label className="sr-only" htmlFor="email">Email</label>
+                    <input
+                      className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                      placeholder="Email address"
+                      type="email"
+                      id="email"
+                    />
+                  </div>
+                  <div>
+                    <label className="sr-only" htmlFor="phone">Phone</label>
+                    <input
+                      className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                      placeholder="Phone Number"
+                      type="tel"
+                      id="phone"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label className="sr-only" htmlFor="message">Message</label>
+                  <textarea
+                    className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                    placeholder="Message"
+                    rows="8"
+                    id="message"
+                  ></textarea>
+                </div>
+                <div className="mt-4">
+                  <button
+                    type="submit"
+                    className="inline-block w-full rounded-lg bg-sky-400 px-5 py-3 font-medium text-white sm:w-auto"
+                  >
+                    Send Inquiry
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default ContactUs;
