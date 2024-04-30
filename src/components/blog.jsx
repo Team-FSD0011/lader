55e63bdb99de0c3fe4a14eeb0b25aedbc14782e6
anitha12042003 <anithas12042003@gmil.com>
2024-04-29 22:37:04 -0700

    jhg

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import image1 from "../components/assets/immigration1.jpg";
 
 const Blog = () => {
   return (
@@ -7,7 +6,7 @@ const Blog = () => {
       <h1 className="font-serif text-gray-700">Blog</h1>
       <div>
         <img
-          className="mt-8 mx-auto w-1/2 h-auto max-w-lg"
+          className="mt-8 mx-auto w-full max-w-lg"
           src="src/components/assets/blog7.jpg"
           alt=""
         />
