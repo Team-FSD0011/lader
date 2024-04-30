2e13a4260a824e9a09376b6c790a68bf4470f82e
anitha12042003 <anithas12042003@gmil.com>
2024-04-30 02:15:47 -0700

    fh

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -4,10 +4,10 @@ import AOS from "aos";
 const Blog = () => {
   return (
     <div className="mt-16 text-center">
-      <h1 className="font-serif text-gray-700 text-#444-200">Blog</h1>
+      <h1 className="font-serif text-gray-700 text-#444-200 text-sm">Blog</h1>
       <div className="">
         <img
-          className="mt-8 mx-auto w-full h-70" // Set the height to h-70 (adjust as needed)
+          className="mt-8 mx-auto w-full h-[500px]" // Set the height to h-70 (adjust as needed)
           src="src/components/assets/blog7.jpg"
           alt=""
         />
