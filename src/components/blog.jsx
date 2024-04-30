09e1d112a382900fd59e8fd6dcd9ac1c8c599604
anitha12042003 <anithas12042003@gmil.com>
2024-04-29 23:06:55 -0700

    gklh

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import './blog.css'
-const Blog = () => 
-{
-    return(
-            
-        <div>
-          <h1 className="header">Blog</h1>
-          <div>
-            <img 
-            className="headerImg"
-            src="src/components/assets/blog7.jpg" alt="" />
-          </div>
-        </div>
-        
-    )
+import AOS from "aos";
 
-}
+const Blog = () => {
+  return (
+    <div className="mt-16 text-center">
+      <h1 className="font-serif text-gray-700">Blog</h1>
+      <div className="">
+        <img
+          className="mt-8 mx-auto w-full h-70" // Set the height to h-70 (adjust as needed)
+          src="src/components/assets/blog7.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
