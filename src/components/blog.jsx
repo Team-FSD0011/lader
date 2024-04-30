fc2ec03a53206725b890a78081a6b6d5ba9ef773
anitha12042003 <anithas12042003@gmil.com>
2024-04-29 22:47:24 -0700

    kh

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import AOS from "aos";
 
 const Blog = () => {
   return (
     <div className="mt-16 text-center">
-      <h1 className="font-serif text-gray-700">Bloggff</h1>
+      <h1 className="font-serif text-gray-700">Blog</h1>
       <div>
         <img
-          className="mt-8 mx-auto w-full max-w-lg"
+          className="mt-8 mx-auto w-full" // Removed max-w-lg, added w-full
           src="src/components/assets/blog7.jpg"
           alt=""
         />
