c7037d3e9b5d208ee16679ae1654918bfc939319
MadhumithaBalasubramani <madhubalu2082001@gmail.com>
2024-04-30 17:32:39 +0530

    dfgh

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -8,15 +8,15 @@ const ContactUs = () => {
     <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8  mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
 	<div className="">
 		<div className="space-y-2">
-			<h2 className="text-4xl font-bold leading-tight mt-4 lg:text-5xl">Let's talk!</h2>
-			
+			<h3 className="text-s  font-semibold italic font-bold leading-tight mt-4 lg:text-xl text-sky-500">Interested in studying overseas with First Ladder? </h3>
+      <h3 className="text-s  font-semibold italic  leading-tight mt-4 lg:text-xl text-sky-500">Fill in your details and we'll call you back!</h3>
 		</div>
-		<img src="src/components/assets/contactform.jpg" alt="" className="p-1 h-80 mt-8 md:h-84" />
+		<img src="src/components/assets/contactform.jpg" alt="" className="p-1 h-80 mt-8 md:h-84 rounded-xl" />
 	</div>
 	<form noValidate="" className="space-y-6">
 		<div>
 			<label htmlFor="name" className="text-sm">Full name</label>
-			<input id="name" type="text" placeholder="" className="w-full border-2 p-3 rounded dark:bg-gray-100" />
+			<input id="name" type="text" placeholder="" className="w-full border-2 p-3 dark:bg-gray-100" />
 		</div>
 		<div>
 			<label htmlFor="email" className="text-sm">Email</label>
