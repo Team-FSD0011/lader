9539d6dff381986a22cfa6ed53f08df298a75e74
MadhumithaBalasubramani <madhubalu2082001@gmail.com>
2024-05-02 17:36:35 +0530

    msg

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import { FaMapMarkedAlt } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 const ContactUs = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -18,45 +19,45 @@ const ContactUs = () => {
     try {
       const response = await axios.post("/api/contact", formData);
       alert(response.data);
-      
     } catch (error) {
       console.error("Error:", error.response.data);
-      
     }
   };
 
   return (
     <>
-      <div className="relative">
-        <h1 className="font-serif text-blue absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl italic z-10">
-          CONTACT US
-        </h1>
-        <img
-          src="/src/components/assets/handshakecontact.jpg"
-          alt=""
-          className="block h-60 w-full opacity-75"
-        />
+      <div>
+        <div class="relative z-20  rounded-xl  ">
+          <h1 class="text-white font-serif text-blue absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl italic z-10">
+            CONTACT US
+          </h1>
+          <img
+            src="/src/components/assets/handshakecontact.jpg"
+            alt=""
+            className="head_img rounded-lg opacity-80"
+          />
+        </div>
       </div>
 
-      <div className="bg-gradient-animation grid max-w-screen-xl grid-cols-1 gap-8 px-8 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800 relative z-20">
-        <div className="relative z-20">
-          <div className="space-y-2">
-            <h3 className="text-lg font-semibold italic font-bold leading-tight mt-4 lg:text-xl text-black-500">
+      <div class="mt-10 bg-gradient-animation grid max-w-screen-xl grid-cols-1 gap-8 px-8 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800 relative z-20">
+        <div class="relative z-20 bg-white rounded-xl shadow-lg">
+          <div class="space-y-2 p-6">
+            <h3 class="text-lg font-semibold italic font-bold leading-tight mt-4 lg:text-xl text-black-500">
               Interested in studying overseas with First Ladder?
             </h3>
-            <h3 className="text-lg font-semibold italic leading-tight mt-4 lg:text-xl text-black-500">
+            <h3 class="text-lg font-semibold italic leading-tight mt-4 lg:text-xl text-black-500">
               Fill in your details and we'll call you back!
             </h3>
           </div>
           <img
             src="/src/components/assets/contactform.jpg"
             alt=""
-            className="p-1 h-80 mt-8 md:h-84 rounded-xl"
+            className="w-70 p-1 shadow-md shadow-blue-400 h-80 mt-2 md:h-84 rounded-xl ml-5"
           />
         </div>
         <form
           noValidate=""
-          className="space-y-6 relative z-20"
+          class="space-y-6 relative z-20 container bg-white rounded-xl shadow-lg p-6"
           onSubmit={handleSubmit}
         >
           <div>
@@ -107,6 +108,36 @@ const ContactUs = () => {
           </button>
         </form>
       </div>
+      <div className="flex">
+      <div className="card mt-10 ml-16 card-content p-5">
+      
+      <FaMapMarkedAlt className="h-16 w-10 text-sky-500" />
+ 
+      <h1 className="text-sky-500 mt-4">Head Quarter</h1><br />
+      <h1>714A, 7th floor, Phase II, Spencer Plaza, Anna Salai, Chennai 600 002.</h1>
+      </div>
+      <div className="card mt-10 ml-24 card-content p-5">
+      <IoMdMail  className="h-16 w-10 text-sky-500" /><br />
+        <h1 className="text-sky-500 mt-4">Mailing Address</h1><br />
+        <h1>hr@firstladder.com</h1>
+      </div>
+      <div className="card mt-10 ml-24 card-content p-5">
+        <h1>phone</h1>
+      </div>
+      </div>
+      <div className="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d62186.64274524278!2d80.1792213!3d13.056972!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5267e0c38c5381%3A0x405ededf7e643c76!2sFirst-Ladder%20Management%20Solutions%20Private%20Limited!5e0!3m2!1sen!2sin!4v1714646303749!5m2!1sen!2sin"
+          width="1300"
+          height="300"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          className="mt-8"
+        ></iframe>
+      </div>
+     
     </>
   );
 };
