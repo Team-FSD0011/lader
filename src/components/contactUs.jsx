cc7e2caf6760897f9c457850b0941120d5ed966e
Prabu1504 <prabupalani1999@gamil.com>
2024-05-02 12:04:35 +0530

    changed

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -41,7 +41,7 @@ const ContactUs = () => {
       <div className="bg-gradient-animation grid max-w-screen-xl grid-cols-1 gap-8 px-8 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800 relative z-20">
         <div className="relative z-20">
           <div className="space-y-2">
-            <h3 className="text-lg font-semibold italic font-bold leading-tight mt-4 lg:text-xl text-black-500">
+            <h3 className="text-lg font-semibold italic  leading-tight mt-4 lg:text-xl text-black-500">
               Interested in studying overseas with First Ladder?
             </h3>
             <h3 className="text-lg font-semibold italic leading-tight mt-4 lg:text-xl text-black-500">
