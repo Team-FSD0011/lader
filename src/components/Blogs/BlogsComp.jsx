36802a905fdb3c3b3bef79a710fea66b189d960a
anitha12042003 <anithas12042003@gmil.com>
2024-05-05 10:02:00 -0700

    fghjk

--- src/components/Blogs/BlogsComp.jsx
+++ src/components/Blogs/BlogsComp.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const BlogsComp = () => {
+  useEffect(() => {
+    AOS.init(); // Initialize AOS
+  }, []);
+
   return (
     <div>
-       <div className='container' data-aos="fade-up">
+      <div className='container' data-aos="fade-up">
         <h1 className='my-8 border-l-8 border-sky-600 py-2 pl-2 text-3xl font-bold'>Our latest Blogs</h1>
-         <div>
-          {
-             BlogsData.map((item, index) =>(
-               <BlogsCard key={index} {...item}/>
-             ))
-          }
-
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default BlogsComp
+export default BlogsComp;
