10cbf55be9a31730beb230d0f046ad1937f4c386
MadhumithaBalasubramani <madhubalu2082001@gmail.com>
2024-05-07 12:25:35 +0530

    change

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { FaMapMarkedAlt } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import { FaPhoneSquareAlt } from "react-icons/fa";
-import handshake from './assets/handshake.jpeg'
+import handshake from "./assets/handshake.jpeg";
 
 const ContactUs = () => {
   const [formData, setFormData] = useState({
@@ -35,8 +35,8 @@ const ContactUs = () => {
             CONTACT US
           </h1>
           <img
-            src="src/components/assets/handshake.jpeg"
-            alt=""
+            src={handshake}
+            alt="Contact image"
             className="head_img rounded-lg opacity-80"
           />
         </div>
@@ -117,26 +117,30 @@ const ContactUs = () => {
           <h1 className="text-sky-500 mt-2 md:mt-4">
             <b>Head Quarter</b>
           </h1>
-          <a href="https://maps.app.goo.gl/b5aD54gnkmN9Ug89A" className="text-sm md:text-base">
-  714A, 7th floor, Phase II, Spencer Plaza, Anna Salai, Chennai 600002
-</a>
-
+          <a
+            href="https://maps.app.goo.gl/b5aD54gnkmN9Ug89A"
+            className="text-sm md:text-base"
+          >
+            714A, 7th floor, Phase II, Spencer Plaza, Anna Salai, Chennai 600002
+          </a>
         </div>
         <div className="card mt-10 md:mt-0 md:ml-16 card-content p-4 md:p-5">
           <IoMdMail className="h-12 w-10 text-sky-500" />
           <h1 className="text-sky-500 mt-2 md:mt-4">
             <b>Mailing Address</b>
           </h1>
-          <a href="mailto:hr@firstladder.com" className="text-sm md:text-base">hr@firstladder.com</a>
-
+          <a href="mailto:hr@firstladder.com" className="text-sm md:text-base">
+            hr@firstladder.com
+          </a>
         </div>
         <div className="card mt-10 md:mt-0 md:ml-16 card-content p-4 md:p-5">
           <FaPhoneSquareAlt className="h-12 w-10 text-sky-500" />
           <h1 className="text-sky-500 mt-2 md:mt-4">
             <b>Business Talk</b>
           </h1>
-          <a href="tel:+918300941219" className="text-sm md:text-base">+91 83009 41219</a>
-
+          <a href="tel:+918300941219" className="text-sm md:text-base">
+            +91 83009 41219
+          </a>
         </div>
       </div>
       <div className="map mt-8">
@@ -154,4 +158,4 @@ const ContactUs = () => {
   );
 };
 
-export default ContactUs;
+export default ContactUs;
