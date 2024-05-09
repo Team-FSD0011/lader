2a04ab7d2aea3236f2a125edb8276a881260243e
janubharathi <bharathisankarias@gmail.com>
2024-05-09 17:05:53 +0530

    comitt

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -8,23 +8,9 @@ import studentsimag4 from "./assets/studentsimag4.png";
 import aaaasd from "./assets/aaaasd.jpg";
 
 const AboutUs = () => {
-<<<<<<< HEAD
-  return (
-    <>
-      <div className="relative">
-        <img src={lkjhj} alt="" className=" h-full ounded-xl object-cover opacity-90 w-full" />
-        <div className="absolute inset-0 text-center font-bold text-5xl md:mt-56">
-          <h1 className="font-serif block rounded-lg p-2 text-white pt-30">ABOUT US</h1>
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row">
-        <img src={welcome1} alt="" className="md:ml-14 h-64 w-80 rounded-xl md:mt-20" />
-        <div className="md:ml-4 md:mr-10">
-          <h1 className="text-3xl font-bold text-center italic md:mt-10 text-sky-500">Welcome to Fisrt Ladder</h1>
-          <p className="text-lg md:mt-5 md:mr-4 md:mr-10 md:ml-5 font-serif text-justify">First-Ladder is an International Education, Immigration, and Recruitment Consulting Company headquartered in Chennai, India. We are a leading global career consulting firm, providing innovative and customized solutions to students, job seekers, and employers. Our process starts with developing an accurate profile of each candidate and using that as our database to match them with the available opportunities in terms of education and jobs. Our aim is to help students and professionals from all over the world to find opportunities that match their requirements, skills, interests, and aspirations, so that they can pursue their while achieving higher returns on investment.</p>
-        </div>
-      </div>
-=======
+
+ 
+
     return (
         <>
             <div className="relative">
@@ -40,7 +26,7 @@ const AboutUs = () => {
                     <p className="text-lg md:mt-5 md:mr-4  md:ml-5 font-serif text-justify">First-Ladder is an International Education, Immigration, and Recruitment Consulting Company headquartered in Chennai, India. We are a leading global career consulting firm, providing innovative and customized solutions to students, job seekers, and employers. Our process starts with developing an accurate profile of each candidate and using that as our database to match them with the available opportunities in terms of education and jobs. Our aim is to help students and professionals from all over the world to find opportunities that match their requirements, skills, interests, and aspirations, so that they can pursue their while achieving higher returns on investment.</p>
                 </div>
             </div>
->>>>>>> 68859edec50e6c9cc0a297dc1299cf8b47dc0226
+
 
       <div className="bg-grey-400">
         <div className="flex">
